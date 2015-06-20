767725ce039119a047e14d0de1510cf5eb2b2ab2
Max Velasco <imaxiv@gmail.com>
2015-06-20 13:33:12 +0200

    Enums for direction and orientation

---
+++ src/main/java/com/mowitnow/enums/Orientation.java
@@ -0,0 +1,14 @@
+package com.mowitnow.enums;
+
+/**
+ * Orientation de la tondeuse
+ *
+ * @author Max Velasco <ivan.velascomartin@gmail.com>
+ */
+public enum Orientation {
+
+    NORTH,
+    SOUTH,
+    WEST,
+    EAST
+}
