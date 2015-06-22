cb528b90c403bd8567f6c6770a547f95566a5baa
Max Velasco <ivan-maximino.velasco-martin@ign.fr>
2015-06-22 14:03:28 +0200

    Read file config as resource

--- src/main/java/com/mowitnow/input/FileCommand.java
+++ src/main/java/com/mowitnow/input/FileCommand.java
@@ -1,7 +1,5 @@
 package com.mowitnow.input;
 
-import com.google.common.collect.Lists;
-import com.mowitnow.enums.Direction;
 import com.mowitnow.exception.BadFormattedInputException;
 import com.mowitnow.input.helper.ConfigHelper;
 import com.mowitnow.land.ILand;
@@ -9,15 +7,15 @@ import com.mowitnow.model.Mow;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 /**
  * Classe pour lire les resources d'un fichier de texte avec les tondeuses, les instructions pour
  * la diriger et les dimensions de la pelouse.
  *
- * On va pour l'exemple et par simplicité charger tous les données en mémoire. On pourra faire
- * une autre implementation de la classe où on va lire directement d'un stream.
- *
+ * On va pour l'exemple et par simplicité charger tous les données en mémoire. On pourra ajouter
+ * des méthodes à la class {@link ConfigHelper}
  * 5 5
  * 1 2 N
  * GAGAGAGAA
@@ -39,12 +37,10 @@ public class FileCommand implements ICommand {
     private ILand land;
 
 
-    public FileCommand(String filePath, ILand land) throws IOException, BadFormattedInputException {
+    public FileCommand(InputStream inputStream, ILand land) throws IOException, BadFormattedInputException {
         this.land = land;
 
-        ClassLoader classLoader = getClass().getClassLoader();
-
-        List<String> lines = IOUtils.readLines(classLoader.getResourceAsStream(filePath));
+        List<String> lines = IOUtils.readLines(inputStream);
         ConfigHelper.setLandDimensions(land, lines);
         this.mows = ConfigHelper.loadMowsAndCommands(lines);
     }
--- src/test/java/com/mowitnow/input/FileCommandTest.java
+++ src/test/java/com/mowitnow/input/FileCommandTest.java
@@ -1,26 +1,51 @@
 package com.mowitnow.input;
 
 
+import com.mowitnow.land.ILand;
+import com.mowitnow.model.Mow;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static com.mowitnow.enums.Direction.FORWARD;
+import static com.mowitnow.enums.Direction.LEFT;
+import static com.mowitnow.enums.Direction.RIGHT;
+import static com.mowitnow.enums.Orientation.EAST;
+import static com.mowitnow.enums.Orientation.NORTH;
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author Max Velasco <ivan.velascomartin@gmail.com>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class FileCommandTest {
 
+    @Test
+    public void testReadFileAndGetMows() throws Exception {
+        ILand land = Mockito.mock(ILand.class);
+        FileCommand command = new FileCommand(getClass().getClassLoader().getResourceAsStream("input.txt"), land);
 
+        List<Mow> mows = command.getMows();
 
-    public void testGetField() {
+        assertThat(mows).hasSize(2);
+        assertThat(mows).hasSize(2);
 
+        assertThat(mows.get(0).getPosition().x).isEqualTo(1);
+        assertThat(mows.get(0).getPosition().y).isEqualTo(2);
+        assertThat(mows.get(0).getOrientation()).isEqualTo(NORTH);
+        assertThat(mows.get(0).getCommands()).containsExactly(
+                LEFT, FORWARD, LEFT, FORWARD, LEFT, FORWARD, LEFT, FORWARD, FORWARD
+        );
+
+        assertThat(mows.get(1).getPosition().x).isEqualTo(3);
+        assertThat(mows.get(1).getPosition().y).isEqualTo(3);
+        assertThat(mows.get(1).getOrientation()).isEqualTo(EAST);
+        assertThat(mows.get(1).getCommands()).containsExactly(
+                FORWARD, FORWARD, RIGHT, FORWARD, FORWARD, RIGHT, FORWARD, RIGHT, RIGHT, FORWARD
+        );
     }
 
-    public void testHasNextMow() {
-
-    }
-
-    public void testNextMow() {
-
-    }
-
-    public void testGetCommands() {
-
-    }
 }
